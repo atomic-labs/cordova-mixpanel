154a1d71bed03374aca5a04fa94628a4745d680c
Rob Eroh <rob@eroh.me>
2014-11-26 10:29:12 -0800

    test hook

--- hooks/after_prepare/generateStaticCallFile.js
+++ hooks/after_prepare/generateStaticCallFile.js
@@ -1,1 +1,19 @@
-console.info(require('path').dirname(require.main.filename));
+/**
+ * @var {Object} console utils
+ */
+var display = {};
+display.success = function (str) {
+    str = '✓  '.green + str;
+    console.log('  ' + str);
+};
+display.error = function (str) {
+    str = '✗  '.red + str;
+    console.log('  ' + str);
+};
+display.header = function (str) {
+    console.log('');
+    console.log(' ' + str.cyan.underline);
+    console.log('');
+};
+
+display.header('Generating Mixpanel A/B Test Statics');
